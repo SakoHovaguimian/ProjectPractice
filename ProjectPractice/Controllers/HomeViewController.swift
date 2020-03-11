e9e019b545f6e503100d1669086796180ef153a3
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2020-03-11 08:40:29 -0700

    -Added CollectionView Logic -Cleaned up codebase

--- ProjectPractice/Controllers/HomeViewController.swift
+++ ProjectPractice/Controllers/HomeViewController.swift
@@ -11,7 +11,7 @@ import Animo
 
 class HomeViewController: UIViewController {
     
-    private lazy var tableView: UITableView = {
+    private lazy var homeTableView: UITableView = {
         let tv = UITableView()
         tv.tableFooterView = UIView()
         tv.delegate = self
@@ -20,12 +20,16 @@ class HomeViewController: UIViewController {
         tv.register(UINib(nibName: HomeTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: HomeTableViewCell.identifier)
         return tv
     }()
+    
+    private lazy var homeCollectionView: UICollectionView =  {
+        return self.configureCollectionView()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .white
-        self.configureTableView()
+        self.configureViews()
 
     }
     
@@ -36,11 +40,17 @@ class HomeViewController: UIViewController {
 
     }
     
-    private func configureTableView() {
+    private func configureViews() {
         
-        self.view.addSubview(self.tableView)
+        self.view.addSubview(self.homeTableView)
         
-        self.tableView.addConstraintsToFillView(self.view)
+        self.homeTableView.anchor(top: self.view.topAnchor, left: self.view.leftAnchor, right: self.view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, height: self.view.frame.height / 2)
+        
+        self.view.addSubview(self.homeCollectionView)
+        
+        self.homeCollectionView.anchor(top: self.homeTableView.bottomAnchor, left: self.view.leftAnchor, bottom: self.view.bottomAnchor, right: self.view.rightAnchor, paddingTop: 15, paddingLeft: 0, paddingBottom: 50, paddingRight: 0)
+        
+        self.view.layoutIfNeeded()
         
     }
     
@@ -49,20 +59,41 @@ class HomeViewController: UIViewController {
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
     }
     
+    private func configureCollectionView() -> UICollectionView {
+       
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.delegate = self
+        cv.dataSource = self
+        cv.backgroundColor = .clear
+        cv.isScrollEnabled = true
+        cv.isPagingEnabled = true
+        cv.showsHorizontalScrollIndicator = false
+        cv.register(UINib(nibName: HomeCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
+        
+        return cv
+        
+    }
+    
 }
 
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if let cell = self.tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell {
+        if let cell = self.homeTableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell {
             
-            cell.value = "Monster MAma Jam"
+            cell.value = "Home Page"
             cell.textLabel?.textColor = .white
             
             return cell
@@ -78,6 +109,30 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.navigationController?.pushViewController(ShopViewController(), animated: true)
+    }
+    
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = self.homeCollectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for: indexPath) as? HomeCollectionViewCell {
+            cell.containerView.backgroundColor = indexPath.row == 0 ? .red : indexPath.row == 1 ? .black : .blue
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionFrame = self.homeCollectionView.frame
+        return CGSize(width: collectionFrame.width, height: collectionFrame.height - 80)
     }
     
 }
